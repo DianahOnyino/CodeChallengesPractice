2e5b3940ad524c5965eef33a853ed9ec82967d6b
donyino <donyino@fh.org>
2019-11-12 20:17:52 +0300

    Solution to counting duplicates

---
+++ counting_duplicates.php
@@ -0,0 +1,27 @@
+<?php 
+/**Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+**/
+
+function duplicateCount($text) {
+    $text_characters = str_split(strtolower($text));
+    $count = 0;
+ 
+    foreach (array_count_values($text_characters) as $text_count) {
+        if($text_count > 1) {
+            $count += 1;
+        } 
+    }
+    return $count;
+}
+
+duplicateCount("Indivisibility");
