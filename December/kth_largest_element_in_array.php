8f344776941b46666ae38be10f8e8ed23dc3f507
donyino <donyino@fh.org>
2019-12-18 10:13:18 +0300

    Solution to Kth largest element in array challenge

---
+++ December/kth_largest_element_in_array.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Find the kth largest element in an unsorted array.
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * Example 1:
+ * Input: [3,2,1,5,6,4] and k = 2
+ * Output: 5
+ * Example 2:
+ *
+ * Input: [3,2,3,1,2,4,5,5,6] and k = 4
+ * Output: 4
+ * Note:
+ * You may assume k is always valid, 1 ≤ k ≤ array's length.
+ */
+class Solution
+{
+    function findKthLargest($nums, $k)
+    {
+        list($max, $max_key) = $this->getNextMax($nums);
+
+        while ($k > 1) {
+            unset($nums[$max_key]);
+            list($max, $max_key) = $this->getNextMax($nums);
+            $k--;
+        }
+
+        return $max;
+    }
+
+    public function getNextMax($nums)
+    {
+        $max = max($nums);
+        $max_key = array_search($max, $nums);
+
+        return array($max, $max_key);
+    }
+}
